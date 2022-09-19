180368c1effc62a2dc11e1e96d82dd687f757ef9
chandredewet <chandredewet@gmail.com>
2022-09-19 19:19:26 +0200

    Dog Gallery api completed with add button

---
+++ challenges/dog-photo-gallery/script.js
@@ -0,0 +1,29 @@
+getImage();
+document.getElementById("btnNext").addEventListener("click", getImage);
+document.getElementById("btnRemove").addEventListener("click", removeImage);
+
+function getImage() {
+  fetch("https://dog.ceo/api/breeds/image/random")
+    .then((response) => {
+      if (response.status >= 200 && response.status <= 299) {
+        return response.json();
+      } else {
+        throw new Error(
+          `Encountered something unexpected: ${response.status} ${response.statusText}`
+        );
+      }
+    })
+    .then((jsonResponse) => {
+      // do whatever you want with the JSON response
+      let dogFirstLi = document.createElement("li");
+      let dogFirstPic = document.createElement("img");
+      dogFirstPic.src = jsonResponse.message;
+      dogFirstPic.alt = "Dog Pic";
+      dogFirstLi.appendChild(dogFirstPic);
+      document.getElementById("photoList").insertBefore(dogFirstLi, referenceNode)appendChild(dogFirstLi);
+    })
+    .catch((error) => {
+      // Handle the error
+      console.log(error);
+    });
+}
